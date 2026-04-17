9fb58e3a4f19736562bda84a0999b9d10fbf3a31
Zeinab <hzazaaa99@gmail.com>
2026-04-17 07:57:57 +0200

    Add file upload functionality in upload.php

--- tp_complementaire/web/upload.php
+++ tp_complementaire/web/upload.php
@@ -1,1 +1,13 @@
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $target = "upload/" . basename($_FILES['file']['name']);
+    move_uploaded_file($_FILES['file']['tmp_name'], $target);
+    echo "Fichier uploadé";
+}
+?>
+
+<form method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="Upload">
+</form>
